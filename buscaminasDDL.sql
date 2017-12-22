b9f020b0d532dca0f6a84b82e13ba3039a367566
jimfede <jimfede@gmail.com>
2017-12-22 19:58:18 -0300

    Listo

--- buscaminasDDL.sql
+++ buscaminasDDL.sql
@@ -1,35 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 11, 2017 at 09:51 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 5.6.32
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE buscaminas; 
+use buscaminas;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `buscaminas`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuarios`
---
-
-create database buscaminas;
-use buscaminas;CREATE TABLE `usuarios` (
+CREATE TABLE `usuarios` (
   `nombreUsuario` varchar(20) NOT NULL,
   `clave` varchar(20) NOT NULL,
   `nombreCompleto` varchar(20) NOT NULL,
@@ -37,6 +11,15 @@ use buscaminas;CREATE TABLE `usuarios` (
   `credito` float DEFAULT '0',
   `usuarioId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`nombreUsuario`, `clave`, `nombreCompleto`, `rol`, `credito`, `usuarioId`) VALUES
+('Daniel', '123', 'Daniel Cisa', 'jugador', 10, 1),
+('Federico', '321', 'Federico Castrillon', 'jugador', 5, 2),
+('Jorge', '123', 'Jorgito', 'administrador', 0, 3);
 
 --
 -- Indexes for dumped tables
@@ -56,9 +39,4 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `usuarioId` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `usuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
